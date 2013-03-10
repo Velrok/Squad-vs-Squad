bb97391e17161684aed54a40cb877b40e6965dd6
Waldemar Schwan <waldemar.schwan@adcloud.com>
2013-03-10 23:56:16 +0100

    clojure project init

---
+++ project.clj
@@ -0,0 +1,11 @@
+(defproject Squad-vs-Squad "0.1.0-SNAPSHOT"
+  :description ""
+  :url "https://github.com/Velrok/Squad-vs-Squad"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [
+    [org.clojure/clojure "1.4.0"]
+    [quil "1.6.0"]
+    [speclj "2.5.0"]]
+  :test-paths ["spec"]
+  :plugins [[speclj "2.5.0"]])
